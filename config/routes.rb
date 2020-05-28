3774ff028b8003de095805dc353ee0cdad2c9ba9
Sehrish Balouch <sehrishbalouch4@gmail.com>
2020-05-28 15:40:49 -0400

    add view files and links to home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  root 'pages#home'
+  root 'pages#home' 
   get 'about', to: 'pages#about'
+  #get 'home', to: 'pages#home'
+  resources :students
+  resources :courses
+  resources :instructors 
 end
