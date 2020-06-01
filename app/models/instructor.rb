1a48f55bbb3538609149e0ce6d50cff0c33c74c8
Sehrish Balouch <sehrishbalouch4@gmail.com>
2020-06-01 15:42:38 -0400

    fix syntax error in instructor model

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,7 +1,8 @@
 class Instructor < ApplicationRecord
+    has_secure_password
     has_many :students
     has_many :courses 
-    has_many :students through: :courses 
+    has_many :students, through: :courses 
     has_one_attached :avatar 
     validates :name, presence: true
     validates :email, presence: true
