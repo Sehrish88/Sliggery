7cd7dad15b8fcc6a045f5aa20c5c0b154f36246c
Sehrish Balouch <sehrishbalouch4@gmail.com>
2020-06-01 15:43:20 -0400

    fix syntax error in student model

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,8 @@
 class Student < ApplicationRecord
+    has_secure_password
     has_many :courses
     has_many :instructors 
-    has_many :instructors through: :courses 
+    has_many :instructors, through: :courses 
     has_one_attached :avatar
     validates :name, presence: true
     validates :email, presence: true
