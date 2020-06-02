710fc2019258ab447107c54299497cd8b9ad0d3c
Sehrish Balouch <sehrishbalouch4@gmail.com>
2020-06-02 16:35:34 -0400

    add show and create actions to controller

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,4 +2,25 @@ class StudentsController < ApplicationController
     def show
         @student = Student.find(params[:id]) 
     end 
+
+    def new
+      @student = Student.new 
+
+    end 
+
+    def create
+        @student = Student.new(student_params)
+        if @student.save
+          session[:student_id] = @student.id
+          redirect_to student_path(@student)
+        else
+          render :new
+        end
+      end
+    
+      private
+    
+      def student_params
+        params.require(:student).permit(:name, :email, :password, :password_confirmation)
+      end
 end
