057b24c15cde45e89733a342cedc0ce6f0e4ebad
Sehrish Balouch <sehrishbalouch4@gmail.com>
2020-06-02 16:41:58 -0400

    add the has_many schedules relationship, remove the has_many instructors relationship

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,9 +1,10 @@
 class Student < ApplicationRecord
+    has_many :schedules 
     has_secure_password
-    has_many :courses
-    has_many :instructors 
+    has_many :courses, through: :schedules
     has_many :instructors, through: :courses 
     has_one_attached :avatar
+    validates_confirmation_of :password
     validates :name, presence: true
     validates :email, presence: true
     validates :email, uniqueness: true 
